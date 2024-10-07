bd9ec0f8b782c05bb20fbf08b42b583593d1e272
eloisamfernandes <eloisafer16@gmail.com>
2024-10-07 01:16:44 -0300

    crossOrigin adicionado

--- src/main/java/com/ufrn/imd/divide/ai/controller/UserController.java
+++ src/main/java/com/ufrn/imd/divide/ai/controller/UserController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/users")
 public class UserController {
