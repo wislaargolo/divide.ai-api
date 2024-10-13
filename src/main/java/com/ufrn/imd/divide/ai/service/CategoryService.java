a058f1e358b90404febfe61149cc407bc8ec1411
eloisamfernandes <eloisafer16@gmail.com>
2024-10-13 19:36:09 -0300

    back de categorias

---
+++ src/main/java/com/ufrn/imd/divide/ai/service/CategoryService.java
@@ -0,0 +1,50 @@
+package com.ufrn.imd.divide.ai.service;
+
+import com.ufrn.imd.divide.ai.model.Category;
+import com.ufrn.imd.divide.ai.repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public Category saveCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public Optional<Category> updateCategory(Long id, Category categoryDetails) {
+        return categoryRepository.findById(id).map(category -> {
+            category.setName(categoryDetails.getName());
+            category.setDescription(categoryDetails.getDescription());
+            category.setColor(categoryDetails.getColor());
+            return categoryRepository.save(category);
+        });
+    }
+
+    public Optional<Category> getCategoryByName(String name) {
+        return categoryRepository.findByName(name);
+    }
+
+    public boolean deleteCategory(Long id) {
+        return categoryRepository.findById(id).map(category -> {
+            categoryRepository.delete(category);
+            return true;
+        }).orElse(false);
+    }
+}
