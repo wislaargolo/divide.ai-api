9552183ab4f4ad4c14e5bacd39ec4d7e9b545e85
eloisamfernandes <eloisafer16@gmail.com>
2024-10-15 00:56:13 -0300

    ajuste de cors

--- src/main/java/com/ufrn/imd/divide/ai/controller/CategoryController.java
+++ src/main/java/com/ufrn/imd/divide/ai/controller/CategoryController.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/categories")
 public class CategoryController {
